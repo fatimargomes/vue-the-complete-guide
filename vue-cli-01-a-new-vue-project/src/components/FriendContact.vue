b5f811e8392379004c428aae5cfc632fe93f6893
fgomes <fraquel.10@gmail.com>
2021-02-18 22:48:42 +0000

    Finish - Section 7 - Components comunication

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toogleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: true,
+      friend: {
+        id: 'joana',
+        name: 'Joana Carvalho',
+        phone: '919191919',
+        email: 'joana@localhost.com'
+      }
+    }
+  },
+  methods: {
+    toogleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
