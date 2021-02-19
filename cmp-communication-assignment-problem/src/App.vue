98efa56b55755c69fd99963d66d9df8e3719a180
fgomes <fraquel.10@gmail.com>
2021-02-19 21:10:16 +0000

    Finished - Section 8 - Components comunication

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <section>
+    <header>
+      <h1>Users</h1>
+    </header>
+    <user-data @add-user="addUser"></user-data>
+    <activeUser
+      v-for="user in users"
+      :key="user.username"
+      :username="user.username"
+      :age="user.age"
+    ></activeUser>
+  </section>
+</template>
+
+<script>
+import ActiveUser from './components/ActiveUser.vue'
+import UserData from './components/UserData.vue'
+export default {
+  components: { ActiveUser, UserData },
+  data() {
+    return {
+      users: [
+        {
+          username: 'fraquel',
+          age: 28
+        },
+        {
+          username: 'jkarlos',
+          age: 25
+        }
+      ]
+    }
+  },
+
+  methods: {
+    addUser(username, age) {
+      const newUser = {
+        username: username,
+        age: +age
+      }
+      this.users.push(newUser)
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+}
+
+#app form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <form @submit.prevent="submitData">
+      <div>
+        <label>Username</label>
+        <input type="text" v-model="enteredUsername" />
+      </div>
+      <div>
+        <label>Age</label>
+        <input type="text" v-model="enteredAge" />
+      </div>
+      <div>
+        <button>Add Contact</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ['add-user'],
+  data() {
+    return {
+      enteredUsername: '',
+      enteredAge: ''
+    }
+  },
+
+  methods: {
+    submitData() {
+      this.$emit('add-user', this.enteredUsername, this.enteredAge)
+    }
+  }
+}
+</script>
