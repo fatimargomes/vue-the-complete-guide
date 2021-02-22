98f3af30efff05c888f95f5fd9c08f25f77e8c6c
fgomes <fraquel.10@gmail.com>
2021-02-22 22:37:16 +0000

    Finished - Section 12 - Sending Http Requests

--- http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -41,6 +41,7 @@
         <p v-if="invalidInput">
           One or more input fields are invalid. Please check your provided data.
         </p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -56,9 +57,10 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null,
     };
   },
-  emits: ['survey-submit'],
+  // emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -67,14 +69,37 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
+
+      this.error = null;
 
       fetch(
-        'https://vue-http-demo-fg-default-rtdb.europe-west1.firebasedatabase.app/surveys.json'
-      );
+        'https://vue-http-demo-fg-default-rtdb.europe-west1.firebasedatabase.app/surveys.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: this.enteredName,
+            rating: this.chosenRating,
+          }),
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            // ...
+          } else {
+            throw new Error('Could not save data!');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.error = error.message;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
