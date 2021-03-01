69c3ef4e44a8c7fec1bbbebb5db75e98775f2974
fgomes <fraquel.10@gmail.com>
2021-03-01 08:27:48 +0000

    WIP - Section 13 - Routinng

--- routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to team 2</router-link>
   </section>
 </template>
 
@@ -26,20 +27,33 @@ export default {
       members: [],
     };
   },
+
+  methods: {
+    loadTeamMembers(route) {
+      const teamId = route.params.teamId;
+
+      const selectedTeam = this.teams.find((team) => team.id === teamId);
+      const members = selectedTeam.members;
+      const selectedMembers = [];
+
+      for (const member of members) {
+        const selectedUser = this.users.find((user) => user.id === member);
+        selectedMembers.push(selectedUser);
+      }
+
+      this.members = selectedMembers;
+      this.teamName = selectedTeam.name;
+    },
+  },
+
   created() {
-    const teamId = this.$route.params.teamId;
+    this.loadTeamMembers(this.$route);
+  },
 
-    const selectedTeam = this.teams.find((team) => team.id === teamId);
-    const members = selectedTeam.members;
-    const selectedMembers = [];
-
-    for (const member of members) {
-      const selectedUser = this.users.find((user) => user.id === member);
-      selectedMembers.push(selectedUser);
-    }
-
-    this.members = selectedMembers;
-    this.teamName = selectedTeam.name;
+  watch: {
+    $route(newRoute) {
+      this.loadTeamMembers(newRoute);
+    },
   },
 };
 </script>
--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,13 +5,33 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue'
+import TeamsFooter from './components/teams/TeamsFooter.vue'
+import UsersFooter from './components/users/UsersFooter.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
-    { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers }
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter
+      },
+      children: [
+        { name: 'team-members', path: ':teamId', component: TeamMembers },
+      ]
+    },
+    {
+      path: '/users',
+      components: {
+        default: UsersList,
+        footer: UsersFooter
+      },
+    },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
 
