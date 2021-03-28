eb523690aec3ed2f2b5122d8779e4d5e8982399a
Fátima Gomes <fraquel.10@gmail.com>
2021-03-28 12:05:54 +0100

    vuex mutations, getters and actions

---
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="increment">Add 1</button>
+  <button @click="increase({ value: 11 })">Add 11</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    // addOne() {
+    //   return this.$store.dispatch('increment');
+    // },
+    ...mapActions(['increment', 'increase']),
+  },
+};
+</script>
